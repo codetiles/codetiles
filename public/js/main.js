1de49d7e528023e3966f1e94b1758a9c1d144e87
Zane <contact@zane.sh>
2018-04-18 23:07:41 -0700

    Implement onLoad() function

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
-function checkCookie(name) {
+let isRegistered;
+
+function onLoad() {
+  if(cookie('user_id') != null) {
+    let user_id = cookie('user_id');
+    document.getElementById('login_widget').style.display = 'none';
+    isRegistered = true;
+    auth();
+  } else {
+    isRegistered = false;
+    document.getElementById('login_widget').style.display = 'inline';
+  }
+}
+
+function cookie(name) {
   if(Cookies.get(name) == null) {
-    return false;
+    return null;
   } else {
     return Cookies.get(name);
   }
@@ -9,7 +23,13 @@ function checkCookie(name) {
 function handleDisplayNameForm() {
   let submitted_displayname = document.getElementById('displayname').value;
 
-  if(checkCookie('user_id') === false) {
-    
-  }
+
+}
+
+function register() {
+  // intended for first time use, when displayname is set for the first time
+}
+
+function auth() {
+  // intended for returning visitors, to authenticate (check if ID is valid)
 }
