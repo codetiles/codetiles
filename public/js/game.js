75926caa3df6f83501b7b6fc0fe64277e5612565
Alex Anderson <alexandersonone@gmail.com>
2018-04-21 01:21:10 -0700

    Add and use kinetic, fix css styles

--- public/js/game.js
+++ public/js/game.js
@@ -2,24 +2,26 @@ $(document).ready(() => {
   function proper_size() {
     var document_height = $(document).height();
     var document_width = $(document).width();
-    $("#content").css("height", document_height);
-    $("#content").css("width", document_width);
-    $("#board").width(document_width*3/5);
-    $("#board").height(document_height);
-    $("#game-board").width($("#board").width()-2);
-    $("#game-board").height($("#board").height()-20);
+    /*
+      $("#content").css("height", document_height);
+      $("#content").css("width", document_width);
+      $("#board").width(document_width);
+      $("#board").height(document_height);
+      $("#game-board").width($("#board").width()-2);
+      $("#game-board").height($("#board").height()-20);
+    */
+    $("#content").kinetic();
   }
   $(document).resize(() => {
     proper_size();
   });
-  var GAME_SIZE = 30;
+  const GAME_SIZE = 30;
   console.log("Ready!");
   var game = $("<table id='game-board'></table>");
   for (var i = 0; i < GAME_SIZE; i++) {
     var new_row = $("<tr></tr>");
     for (var j = 0; j < GAME_SIZE; j++) {
       var new_column = $("<td></td>");
-      new_column.css("background-color", "#404e7c");
       //new_column.css("border-radius", "15%");
       new_row.append(new_column);
     }
