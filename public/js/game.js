8e37bbeb0d90ac88c463bb644a66078ed58e9e8c
Yev Barkalov <yev@localhost.localdomain>
2018-04-20 23:54:00 -0700

    did some stuff woot woot

--- public/js/game.js
+++ public/js/game.js
@@ -4,21 +4,23 @@ $(document).ready(() => {
     var document_width = $(document).width();
     $("#content").css("height", document_height);
     $("#content").css("width", document_width);
-    $("#board").width(document_width/2);
+    $("#board").width(document_width*3/5);
     $("#board").height(document_height);
     $("#game-board").width($("#board").width()-2);
-    $("#game-board").height($("#board").height()-30);
+    $("#game-board").height($("#board").height()-20);
   }
   $(document).resize(() => {
     proper_size();
   });
-  var GAME_SIZE = 10;
+  var GAME_SIZE = 30;
   console.log("Ready!");
   var game = $("<table id='game-board'></table>");
-  for (var i = 0; i < 4*GAME_SIZE; i++) {
+  for (var i = 0; i < GAME_SIZE; i++) {
     var new_row = $("<tr></tr>");
-    for (var j = 0; j < 2*GAME_SIZE; j++) {
+    for (var j = 0; j < GAME_SIZE; j++) {
       var new_column = $("<td></td>");
+      new_column.css("background-color", "#404e7c");
+      //new_column.css("border-radius", "15%");
       new_row.append(new_column);
     }
     game.append(new_row);
