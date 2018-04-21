e48dcbeae37d9d2857ebb12c2e634a68f8bce213
Alex Anderson <alexandersonone@gmail.com>
2018-04-21 01:30:38 -0700

    Make boards randomly generate with color and number

--- public/js/game.js
+++ public/js/game.js
@@ -24,6 +24,24 @@ $(document).ready(() => {
       var new_column = $("<td></td>");
       //new_column.css("border-radius", "15%");
       new_row.append(new_column);
+      if (Math.random() > .9) {
+        new_column.text(function() {
+          return Math.floor(Math.random() * 100)
+        });
+        new_column.addClass("blue");
+      }
+      if (Math.random() > .9) {
+        new_column.text(function() {
+          return Math.floor(Math.random() * 100)
+        });
+        new_column.addClass("red");
+      }
+      if (Math.random() > .9) {
+        new_column.text(function() {
+          return Math.floor(Math.random() * 100)
+        });
+        new_column.addClass("yellow");
+      }
     }
     game.append(new_row);
   }
