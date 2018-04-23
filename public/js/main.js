0c06cbbfd2923771b00ae0c0e2e5545fc881912e
Zane <contact@zane.sh>
2018-04-22 17:45:33 -0700

    Escape strings to prevent XSS

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ function onLoad() {
   // check if localStorage isn't supported
   if(!window.localStorage) window.location.href = '/unsupported';
 
-  if(localStorage.getItem('user_id') != null) {
+  if(encodeURIComponent(localStorage.getItem('user_id')) != null) {
     document.getElementById('login_widget').style.display = 'none';
     isRegistered = true;
     auth();
@@ -18,7 +18,7 @@ function onLoad() {
 
 // register user/re-authenticate user on submission of display name
 function handleDisplayNameSubmission() {
-  let submitted_displayname = document.getElementById('displayname').value;
+  let submitted_displayname = encodeURIComponent(document.getElementById('displayname').value);
   if(!isRegistered) {
     register(submitted_displayname);
   } else if(isRegistered) {
@@ -40,13 +40,13 @@ function register(displayname) {
 
 // intended for returning visitors, to authenticate (check if ID is valid)
 function auth() {
-  $.getJSON("api/v1/verifyuser/"+localStorage.getItem('user_id'), function(data) {
+  $.getJSON("api/v1/verifyuser/"+encodeURIComponent(localStorage.getItem('user_id')), function(data) {
     if(data.Exists === "false") {
       localStorage.removeItem('user_id');
       document.getElementById('login_widget').style.display = 'inline';
     } else if(data.Exists === "true") {
-      user_id = localStorage.getItem('user_id');
-      displayname = data.DisplayName;
+      user_id = encodeURIComponent(localStorage.getItem('user_id'));
+      displayname = encodeURIComponent(data.DisplayName);
       document.getElementById('logged_in_text').innerHTML = ('Logged in as '+displayname+', <a href="" onclick="logout();">logout</a>');
     }
   })
