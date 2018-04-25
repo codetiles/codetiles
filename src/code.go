e55cb75e9192111efd8b2e630a195d27dda049af
Alex Anderson <alexandersonone@gmail.com>
2018-04-24 20:47:07 -0700

    Run gofmt

--- src/code.go
+++ src/code.go
@@ -9,7 +9,7 @@ import (
 
 // Handle a person uploading their code
 func handleUploadCode(w http.ResponseWriter, r *http.Request) {
-  // Get user's JSON
+	// Get user's JSON
 	var userJson map[string]string
 
 	err := json.NewDecoder(r.Body).Decode(&userJson)
@@ -20,67 +20,67 @@ func handleUploadCode(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  // Process id and code
+	// Process id and code
 	id, existsId := userJson["id"]
 	rawCode, existsCode := userJson["code"]
 
-  // If a value is missing, the request didn't meet the requirements
+	// If a value is missing, the request didn't meet the requirements
 	if !existsId || !existsCode {
 		rj, _ := json.Marshal(map[string]string{
 			"Success": "false",
-			"Reason" : "JSON is missing essential key value.",
+			"Reason":  "JSON is missing essential key value.",
 		})
 
-    w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 		io.WriteString(w, string(rj))
 		return
 	}
 
-  // Process the user's code for errors
+	// Process the user's code for errors
 	ver, errstr := verifyCode(rawCode)
 
-  // If an error exists, don't change the code. Send them a reason as well.
+	// If an error exists, don't change the code. Send them a reason as well.
 	if !ver {
 		w.WriteHeader(http.StatusNotAcceptable)
 
 		rj, _ := json.Marshal(map[string]string{
-			"Success" : "false",
-			"Reason" : errstr,
+			"Success": "false",
+			"Reason":  errstr,
 		})
 
 		io.WriteString(w, string(rj))
 		return
 	}
 
-  // Write to user's code
-  var uid [8]byte
-  copy(uid[:], []byte(id))
-  usersArrayLock.RLock()
-  _, ex := users[uid]
-  usersArrayLock.RUnlock()
+	// Write to user's code
+	var uid [8]byte
+	copy(uid[:], []byte(id))
+	usersArrayLock.RLock()
+	_, ex := users[uid]
+	usersArrayLock.RUnlock()
 
-  // Check if user exists
-  if !ex {
+	// Check if user exists
+	if !ex {
 		rj, _ := json.Marshal(map[string]string{
-			"Success" : "false",
-			"Reason" : "Id not found",
+			"Success": "false",
+			"Reason":  "Id not found",
 		})
 
 		io.WriteString(w, string(rj))
 		return
-  }
+	}
 
-  usersArrayLock.Lock()
-  user := users[uid]
-  user.code = rawCode
-  usersArrayLock.Unlock()
+	usersArrayLock.Lock()
+	user := users[uid]
+	user.code = rawCode
+	usersArrayLock.Unlock()
 
-  rj, _ := json.Marshal(map[string]string{
-    "Success" : "true",
-    "Reason" : "",
-  })
+	rj, _ := json.Marshal(map[string]string{
+		"Success": "true",
+		"Reason":  "",
+	})
 
-  io.WriteString(w, string(rj))
+	io.WriteString(w, string(rj))
 }
 
 // Fuction for verifing code (confirming validity)
