63c139f7553735942e318e53c6c16b28d2ca3177
Alex Anderson <alexandersonone@gmail.com>
2018-04-27 12:21:15 -0700

    Equal distribution of colors

--- public/js/game.js
+++ public/js/game.js
@@ -13,24 +13,23 @@ $(document).ready(() => {
       var new_column = $("<td></td>");
       //new_column.css("border-radius", "15%");
       new_row.append(new_column);
-      if (Math.random() > .2) {
+      if (Math.random() > .9) {
         new_column.text(function() {
-          return Math.floor(Math.random() * 100)
+          return Math.floor(Math.random() * 100 + 1)
         });
-        new_column.addClass("blue");
+        var x = Math.random()
+        if (x > .66) {
+          new_column.addClass("blue");
+        }
+        else if (x < .33) {
+          new_column.addClass("red")
+        }
+        else {
+          new_column.addClass("yellow")
+        }
+
       }
-      if (Math.random() > .3) {
-        new_column.text(function() {
-          return Math.floor(Math.random() * 100)
-        });
-        new_column.addClass("red");
-      }
-      if (Math.random() > .5) {
-        new_column.text(function() {
-          return Math.floor(Math.random() * 100)
-        });
-        new_column.addClass("yellow");
-      }
+
     }
     game.append(new_row);
   }
