e1f145f5aa32eafc4c86a9f87555f32062719ef8
Alex Anderson <alexandersonone@gmail.com>
2018-04-29 22:06:50 -0700

    Reflect change in JS

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8080/api/v1/findgame/ws');
+const socket = new WebSocket('ws://localhost:8080/api/v1/ws/findgame');
 
 socket.addEventListener('message', function (event) {
   console.log('Message from server ', event.data);
