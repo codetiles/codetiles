577bafff7346d348e8e15f01c555515b536a9b52
Zane <contact@zane.sh>
2018-04-30 17:17:22 -0700

    Redirect to game on start

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,9 +1,15 @@
 const socket = new WebSocket('ws://localhost:8080/api/v1/ws/findgame');
 
 socket.addEventListener('message', function (event) {
+  // user isn't logged in
   if (event.data == "User id does not exist") {
     window.location.href = "/"
   }
+  // game has started, redirect
+  if (event.data == "...") {
+    window.location.href = "/game"
+  }
+  // show current message as text
   $("#status").text(function(){
     return event.data
   })
@@ -13,6 +19,7 @@ socket.addEventListener('open', function (event) {
   socket.send(localStorage.getItem('user_id'));
 });
 
+// keep connection alive
 setInterval(function(){
   socket.send("...")
 }, 250);
