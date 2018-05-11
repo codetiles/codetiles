ef129b7ff4a51145ec7ee99ee30cf81df78c1cfd
Zane <contact@zane.sh>
2018-05-10 20:39:38 -0700

    Don't render board by default

--- public/js/game.js
+++ public/js/game.js
@@ -18,21 +18,6 @@ $(document).ready(() => {
     for (var j = 0; j < GAME_SIZE; j++) {
       var new_column = $("<td></td>");
       new_row.append(new_column);
-      if (Math.random() > .9) {
-        new_column.text(function() {
-          return Math.floor(Math.random() * 100 + 1)
-        });
-        var x = Math.random()
-        if (x > .66) {
-          new_column.addClass("blue");
-        }
-        else if (x < .33) {
-          new_column.addClass("red")
-        }
-        else {
-          new_column.addClass("yellow")
-        }
-      }
     }
     game.append(new_row);
     $(document).delegate('#editor', 'keydown', function(e) {
